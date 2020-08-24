969ae5733e964f3a411ea7013d36cbf6c9aad9d9
wlukla <vladislav.danilochkin@gmail.com>
2020-08-24 12:59:52 +0300

    feat: start adding test case for HandleUsersCreate

---
+++ internal/app/apiserver/server_internal_test.go
@@ -0,0 +1,19 @@
+package apiserver
+
+import (
+	"golang-http-rest-api/internal/app/store/teststore"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServer_HandleUsersCreate(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	s := newServer(teststore.New())
+	s.ServeHTTP(rec, req)
+
+	assert.Equal(t, rec.Code, http.StatusOK)
+}
